a6337a5fdae614b894de223fbdc9a9024c4ca500
Acheron <acheron.igor.b@gmail.com>
2017-04-23 16:20:52 +0300

    Added duplicate detection. Fixed error when chose 1 page to scrap

--- homework_6/alchemy/model.py
+++ homework_6/alchemy/model.py
@@ -11,7 +11,7 @@ class Posts(Base):
     id = Column(Integer, primary_key=True)
     author = Column(String(40))
     url = Column(String(100), unique=True)
-    topic = Column(String, unique=True)
+    topic = Column(String)
     article_text = Column(Text)
     price = Column(String, nullable=True)
     currency = Column(String(10))
