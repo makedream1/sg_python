841c6db5325750ff500fa6e2b32a8e5b504baa4c
makedream1 <makedream@bk.ru>
2017-04-26 22:26:46 +0300

    update db models (add table Authors)

--- homework_6/alchemy/model.py
+++ homework_6/alchemy/model.py
@@ -1,20 +1,29 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Column, MetaData
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
+
+
+class Authors(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    author = Column(String(40), unique=True)
 
 
 class Posts(Base):
     __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    author = Column(String(40))
+    author_id = Column(Integer, ForeignKey('authors.id'))
     url = Column(String(100), unique=True)
     topic = Column(String)
     article_text = Column(Text)
     price = Column(String, nullable=True)
     currency = Column(String(10))
+
+    author = relationship("Authors", backref="posts")
 
 
 if __name__ == '__main__':
